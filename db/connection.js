8d02d4a6f8abcf74856458a03e2d37ea0d83cd65
Tom Hawes <twhawes@gmail.com>
2019-12-04 13:53:48 +0000

    testing commit to feature branch

--- db/connection.js
+++ db/connection.js
@@ -4,3 +4,5 @@ const DB_URL = 'mongodb://localhost:27017/project-bhilt';
 mongoose.connect(DB_URL)
     .then(() => console.log(`Connected to database ${DB_URL}`))
     .catch(console.log);
+
+    //checking git push

3663479407dff37dcd65d453b4c7cf20c3199d1f
Tom Hawes <twhawes@gmail.com>
2019-12-05 11:49:52 +0000

    remove unnecessary file

--- db/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/project-bhilt';
-
-mongoose.connect(DB_URL)
-    .then(() => console.log(`Connected to database ${DB_URL}`))
-    .catch(console.log);
-
-    //checking git push

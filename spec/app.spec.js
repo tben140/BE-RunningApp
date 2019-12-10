1b70db17184715efec897cacea690a374ca5c190
Tombo <53969229+tombo-coys@users.noreply.github.com>
2019-12-10 14:54:19 +0000

    add correct keys to post new user test (#14)

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -114,32 +114,30 @@ describe('/pollution-points', () => {
 
 });
 describe('/users POST', () => {
-  it('returns an object with the new user', () => {
-    const randomName = faker.name.findName();
-    return request(app)
-      .post('/api/users')
-      .send({
-        username: `${randomName}`,
-        email: 'user@gmail.com',
-        password: 'dsfdfdfdfdfd',
-        current_location: '53.4860211, -2.2397307'
-      })
-      .expect(201)
-      .then(({ body }) => {
-        expect(body).to.be.an('object');
-        expect(body.user).to.have.keys(
-          'username',
-          'email',
-          'password',
-          'current_location',
-          '_id',
-          '__v'
-        );
-      });
-  });
+    it.only('returns an object with the new user', () => {
+        const randomName = faker.name.findName();
+        return request(app)
+            .post('/api/users')
+            .send({
+                username: `${randomName}`,
+                email: 'user@gmail.com',
+                password: 'dsfdfdfdfdfd',
+                current_location: '53.4860211, -2.2397307',
+                end_location: {
+                    lat: 0,
+                    long: 0
+                }
+            })
+            .expect(201)
+            .then(({ body }) => {
+                console.log(body)
+                expect(body).to.be.an('object');
+                expect(body.user).to.have.keys('username', 'email', 'password', 'current_location', '_id', '__v', 'end_location');
+            })
+    });
 });
-describe('/users PATCH', () => {
 
+describe('/users PATCH', () => {
   it('updates the users geo-location', () => {
     return request(app)
       .patch('/api/users')

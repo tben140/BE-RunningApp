ee1ba28cf6b5ee651e44e7bdb5cbac2514ec830c
tben140 <tben140@gmail.com>
2020-05-19 02:56:15 +0100

    Add mongoose connect error console log

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
-const mongoose = require("mongoose");
-const apiRouter = require("./routes/apiRouter");
-const { customErrors, allOtherErrors } = require('./utils/errorHandler')
-
+const mongoose = require('mongoose');
+const apiRouter = require('./routes/apiRouter');
+const { customErrors, allOtherErrors } = require('./utils/errorHandler');
 
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 // mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 
-const url = 'mongodb+srv://test:test@cluster0-frwlz.gcp.mongodb.net/test?retryWrites=true&w=majority'
-mongoose.connect(url, { useNewUrlParser: true });
+const url =
+  'mongodb+srv://test:test@cluster0-frwlz.gcp.mongodb.net/test?retryWrites=true&w=majority';
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .catch((error) => console.log('MONGOOSE CONNECT ERROR ->>', error));
 
 app.listen(port, () => {
   console.log(`App Listening on ${port}`);
 });
-app.use(customErrors)
-app.use(allOtherErrors)
+app.use(customErrors);
+app.use(allOtherErrors);
 
 module.exports = { app };
